eebae8feea8ad7e5d136149a41fe0a8dfcc0baec
Avinash kumar <jhaclsxn8126@gmail.com>
2025-12-24 10:45:30 +0530

    merged the updated frontend code

--- frontend/src/pages/MyAppointments.jsx
+++ frontend/src/pages/MyAppointments.jsx
@@ -11,7 +11,7 @@ const MyAppointments = () => {
       </p>
 
       <div>
-        {doctors.slice(0, 3).map((item, index) => (
+        {doctors.slice(0, 2).map((item, index) => (
           <div
             className="grid grid-cols-[1fr_2fr] gap-4 sm:flex sm:gap-6 py-2 border-b"
             key={index}
