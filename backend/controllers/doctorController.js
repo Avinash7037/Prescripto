a7498a2263bcfc08e59132b9ea987ae3b16c71ce
Avinash kumar <jhaclsxn8126@gmail.com>
2025-12-25 10:45:13 +0530

    created api for doctor and admin

---
+++ backend/controllers/doctorController.js
@@ -0,0 +1,17 @@
+// API for adding Doctor
+
+const addDoctor = async (req, res) => {
+  try {
+    const {
+      name,
+      email,
+      password,
+      speciality,
+      degree,
+      experience,
+      about,
+      fees,
+      address,
+    } = req.body;
+  } catch (error) {}
+};
