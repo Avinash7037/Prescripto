aad265df87a5096da76b958f8f9a9f71a97d3f7c
Avinash kumar <jhaclsxn8126@gmail.com>
2025-12-26 10:58:10 +0530

    created admin panel with login and logout feature

--- admin/src/App.jsx
+++ admin/src/App.jsx
@@ -1,12 +1,23 @@
-import React from "react";
+import React, { useContext } from "react";
 import Login from "./pages/login";
 import { Route, Routes } from "react-router-dom";
+import { ToastContainer, toast } from "react-toastify";
+import "react-toastify/dist/ReactToastify.css";
+import { AdminContext } from "./context/AdminContext";
+import Navbar from "./components/Navbar";
 
 const App = () => {
-  return (
-    <Routes>
-      <Route path="/" element={<Login />} />
-    </Routes>
+  const { aToken } = useContext(AdminContext);
+  return aToken ? (
+    <div className="bg-[#F8F9FD]">
+      <ToastContainer />
+      <Navbar />
+    </div>
+  ) : (
+    <>
+      <Login />
+      <ToastContainer />
+    </>
   );
 };
 
--- admin/src/pages/Login.jsx
+++ admin/src/pages/Login.jsx
@@ -1,10 +1,40 @@
-import React, { useState } from "react";
+import React, { useContext, useState } from "react";
+import { AdminContext } from "../context/AdminContext";
+import axios from "axios";
+import { toast } from "react-toastify";
 
 const Login = () => {
   const [state, setState] = useState("Admin");
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+
+  const { setAToken, backendUrl } = useContext(AdminContext);
+
+  const onSubmitHandler = async (event) => {
+    event.preventDefault();
+
+    try {
+      const { data } = await axios.post(backendUrl + "/api/admin/login", {
+        email,
+        password,
+      });
+
+      console.log(data);
+
+      if (data.success) {
+        console.log(data.token);
+        localStorage.setItem("aToken", data.token);
+        setAToken(data.token);
+      } else {
+        toast.error(data.message);
+      }
+    } catch (error) {
+      console.log(error);
+    }
+  };
 
   return (
-    <form className="min-h-[80vh] flex items-center">
+    <form onSubmit={onSubmitHandler} className="min-h-[80vh] flex items-center">
       <div className="flex flex-col gap-3 m-auto items-start p-8 min-w-[340px] sm:min-w-96 border rounded-xl text-[#5E5E5E] text-sm shadow-lg">
         <p className="text-2xl font-semibold m-auto">
           <span className="text-primary">{state}</span> Login
@@ -13,26 +43,35 @@ const Login = () => {
         <div className="w-full">
           <p>Email</p>
           <input
-            className="border border-[#DADADA] rounded w-full p-2 mt-1"
             type="email"
+            value={email}
+            onChange={(e) => setEmail(e.target.value)}
+            className="border border-[#DADADA] rounded w-full p-2 mt-1"
             required
           />
         </div>
+
         <div className="w-full">
           <p>Password</p>
           <input
-            className="border border-[#DADADA] rounded w-full p-2 mt-1"
             type="password"
+            value={password}
+            onChange={(e) => setPassword(e.target.value)}
+            className="border border-[#DADADA] rounded w-full p-2 mt-1"
             required
           />
         </div>
-        <button className="bg-primary text-white w-full py-2 rounded-md text-base ">
+
+        <button
+          type="submit"
+          className="bg-primary text-white w-full py-2 rounded-md text-base cursor-pointer "
+        >
           Login
         </button>
 
         {state === "Admin" ? (
           <p>
-            Doctor Login?
+            Doctor Login?{" "}
             <span
               className="text-primary underline cursor-pointer"
               onClick={() => setState("Doctor")}
@@ -42,7 +81,7 @@ const Login = () => {
           </p>
         ) : (
           <p>
-            Admin Login?
+            Admin Login?{" "}
             <span
               className="text-primary underline cursor-pointer"
               onClick={() => setState("Admin")}
