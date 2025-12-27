914f1d4f1c7f2d670eed8e8da65d3df7032ae4e2
Avinash kumar <jhaclsxn8126@gmail.com>
2025-12-27 17:47:58 +0530

    fixed bug in getprofile api

--- backend/middlewares/authUser.js
+++ backend/middlewares/authUser.js
@@ -1,25 +1,19 @@
 import jwt from "jsonwebtoken";
 
 // user authentication middleware
-const authAdmin = async (req, res, next) => {
+const authUser = async (req, res, next) => {
   try {
-    const { atoken } = req.headers;
+    const { token } = req.headers;
 
-    if (!atoken) {
+    if (!token) {
       return res.json({
         success: false,
         message: "Not Authorized Login Again",
       });
     }
 
-    const token_decode = jwt.verify(atoken, process.env.JWT_SECRET);
-
-    if (token_decode !== process.env.ADMIN_EMAIL + process.env.ADMIN_PASSWORD) {
-      return res.json({
-        success: false,
-        message: "Not Authorized Login Again",
-      });
-    }
+    const token_decode = jwt.verify(token, process.env.JWT_SECRET);
+    req.userId = token_decode.id;
 
     next();
   } catch (error) {
@@ -31,4 +25,4 @@ const authAdmin = async (req, res, next) => {
   }
 };
 
-export default authAdmin;
+export default authUser;
