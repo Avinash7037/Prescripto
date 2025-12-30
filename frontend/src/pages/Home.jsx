eb0f113e35fffd15b8bced405cc60671b6cd835d
Avinash kumar <jhaclsxn8126@gmail.com>
2025-12-30 21:10:30 +0530

    Fix Header component case sensitivity

--- frontend/src/pages/Home.jsx
+++ frontend/src/pages/Home.jsx
@@ -1,5 +1,5 @@
 import React from "react";
-import Header from "../components/HeaderTemp";
+import Header from "../components/Header";
 import SpecialityMenu from "../components/SpecialityMenu";
 import TopDoctors from "../components/TopDoctors";
 import Banner from "../components/Banner";
