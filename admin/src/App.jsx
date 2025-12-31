0a99775511a033736812fccc747f18b5738c31fe
Avinash kumar <jhaclsxn8126@gmail.com>
2025-12-31 11:46:26 +0530

    issue in DoctorAppointment

--- admin/src/App.jsx
+++ admin/src/App.jsx
@@ -12,7 +12,7 @@ import AddDoctor from "./pages/Admin/AddDoctor";
 import DoctorsList from "./pages/Admin/DoctorsList";
 import { DoctorContext } from "./context/DoctorContext";
 import DoctorDashboard from "./pages/Doctor/DoctorDashboard";
-import DoctorAppointments from "./pages/Doctor/doctorAppointments";
+import DoctorAppointments from "./pages/Doctor/DoctorAppointmentsTemp";
 import DoctorProfile from "./pages/Doctor/DoctorProfile";
 
 const App = () => {
