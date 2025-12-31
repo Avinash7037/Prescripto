e65d96bd45ebe864c5ad20ef90e90b413e354f4f
Avinash kumar <jhaclsxn8126@gmail.com>
2025-12-31 11:26:42 +0530

    fixed

--- frontend/src/components/Navbar.jsx
+++ frontend/src/components/Navbar.jsx
@@ -3,7 +3,6 @@ import { assets } from "../assets/assets";
 import { NavLink, useNavigate } from "react-router-dom";
 import { useContext } from "react";
 import { AppContext } from "../context/AppContext";
-import { DoctorContext } from "../../../admin/src/context/DoctorContext";
 
 const Navbar = () => {
   const [showMenu, setShowMenu] = useState(false);
